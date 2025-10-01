4c7ecb1b611a27c3b9e8ccab7d8a2c87809f8ec4
Erick Lopez <ecklpz@gmail.com>
2025-09-30 21:37:11 -0500

    chore: initial commit

---
+++ src/test/java/com/racoonsfinds/backend/RacoonsfindsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racoonsfinds.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacoonsfindsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
